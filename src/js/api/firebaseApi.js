9f245206f0dc2ae5b786a629e842bd1a64610dfb
BrOrlandi <brorlandi@gmail.com>
2018-05-19 02:53:57 -0300

    WIP firebase api.

---
+++ src/js/api/firebaseApi.js
@@ -0,0 +1,89 @@
+import { firebases, defaultFirebase, defaultFirebasePrefix } from './config';
+import Http from './Http';
+
+const SERVER_TIMESTAMP = { '.sv': 'timestamp' };
+
+export const _parseFirebasekey = (key) => {
+  const result = {
+    baseURL: defaultFirebase,
+  };
+
+  // is creating a new opening
+  if (!key) {
+    return result;
+  }
+
+  const prefix = key[0];
+  const firebaseUrl = firebases[prefix];
+
+  if (!firebaseUrl) {
+    throw new Error(`Firebase url for prefix not set: ${prefix}`);
+  }
+
+  result.baseURL = firebaseUrl;
+  result.key = key.substr(1);
+
+  return result;
+};
+
+export const _parseSpecialKeys = (key) => {
+  switch (key) {
+    case 'Season1':
+      return '----';
+    // TODO other season
+    default:
+      return key;
+  }
+};
+
+const openingsCache = {};
+
+export const _generateUrlWithKey = (key) => {
+  const openingPrefix = '/openings/';
+  return `${openingPrefix}-${key}.json`;
+};
+
+
+// export const fetchKey = async (initialKey) => {
+//   const openingFromCache = openingsCache[initialKey];
+//   if (openingFromCache) {
+//     Raven.captureBreadcrumb({
+//       message: 'Getting intro from cache.',
+//       category: 'info',
+//       data: openingFromCache,
+//     });
+//     return openingFromCache;
+//   }
+
+//   const rawkey = _parseSpecialKeys(initialKey);
+//   const { baseURL, key } = _parseFirebasekey(rawkey);
+//   const http = Http(baseURL);
+
+//   const url = _generateUrlWithKey(key);
+
+//   Raven.captureBreadcrumb({
+//     message: 'Loading intro from Firebase.',
+//     category: 'info',
+//     data: { initialKey },
+//   });
+//   const response = await http.get(url);
+//   const opening = response.data;
+//   // const opening = {"center":true,"episode":"Episode VIII","intro":"Kassel Labs","logo":"kassel\nlabs","text":"Kassel Labs\n\nkassel\nlabs\n\nKASSEL LABS\n\nKASSEL\nLABS\n\nkassel labs","title":"KASSEL LABS"};
+//   if (!opening) {
+//     const error = new Error(`Opening not found: ${initialKey}`);
+//     Raven.captureException(error);
+//   }
+//   openingsCache[initialKey] = opening;
+//   return opening;
+// };
+
+
+export const saveOpening = async (opening) => {
+  const http = Http(defaultFirebase);
+
+  opening.created = SERVER_TIMESTAMP;
+
+  const response = await http.post('/openings.json', opening);
+  const key = `${defaultFirebasePrefix}${response.data.name.substr(1)}`;
+  return key;
+};
