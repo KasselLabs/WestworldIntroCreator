9938f2b40f31f18ba616a51cacc385d342ec1d25
BrOrlandi <brorlandi@gmail.com>
2018-06-14 00:52:22 -0300

    Mock opening.

--- src/js/api/firebaseApi.js
+++ src/js/api/firebaseApi.js
@@ -68,7 +68,8 @@ export const fetchKey = async (initialKey) => {
   });
   const response = await http.get(url);
   const opening = response.data;
-  // const opening = {"center":true,"episode":"Episode VIII","intro":"Kassel Labs","logo":"kassel\nlabs","text":"Kassel Labs\n\nkassel\nlabs\n\nKASSEL LABS\n\nKASSEL\nLABS\n\nkassel labs","title":"KASSEL LABS"};
+  // const opening = {"created":1528165309973,"texts":{"text0":"EVAN RACHEL WOOD","text1":"THANDIE NEWTOW","text10":"SHANNON WOODWARD","text11":"WITH\nED HARRIS","text12":"AND\nANTHONY HOPKINS","text13":"MUSIC BY\nRAMIN DJAWADI","text14":"EDITED BY\nSTEPHEN SMEL, ACE\nMARC JOZEFOMICZ","text15":"PRODUCTION DESIGNER\nNATHAN CROWLEY","text16":"DIRECTOR OF PHOTOGRAPHY\nPAUL CAMERON, ASC","text17":"CO-PRODUCER\nBRUCE DUNN","text18":"CO-PRODUCER\nSUSAN EKINS","text19":"CO-PRODUCER\nSTEPHEN SEMEL","text2":"JEFFREY WRIGHT","text20":"CO-EXECUTIVE PRODUCER\nDAVID COATSWORTH","text21":"CO-EXECUTIVE PRODUCER\nKATHY LINGG","text22":"CO-EXECUTIVE PRODUCER\nATHENA WICKHAM","text23":"EXECUTIVE PRODUCER\nBRYAN BURK","text24":"EXECUTIVE PRODUCER\nJERRY WEINTRAUB","text25":"EXECUTIVE PRODUCER\nLISA JOY","text26":"EXECUTIVE PRODUCER\nJONATHAN NOLAN","text27":"EXECUTIVE PRODUCER\nJ.J. ABRAMS","text28":"WESTWORLD","text29":"CREATED FOR TELEVISION BY\nJONATHAN NOLAN & LISA JOY","text3":"JAMES MARSDEN","text30":"BASED ON THE FILM WRITTEN BY\nMICHAEL CRICHTON","text31":"TELEPLAY BY\nJONATHAN NOLAN & LISA JOY","text32":"STORY BY\nJONATHAN NOLAN & LISA JOY\nAND\nMICHAEL CRICHTON","text33":"DIRECTED BY\nJONATHAN NOLAN","text4":"INGRID BOLSØ BERDAL","text5":"LUKE HEMSWORTH","text6":"SIDSE BABETT KNUDSEN","text7":"SIMON QUARTERMAN","text8":"RODRIGO SANTORO","text9":"ANGELA SARAFYAN"}};
+
   if (!opening) {
     const error = new Error(`Opening not found: ${initialKey}`);
     Raven.captureException(error);
