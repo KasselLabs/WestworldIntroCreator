12619892ae7acc25d3e9333870a75290a41396d9
BrOrlandi <brorlandi@gmail.com>
2018-06-16 22:17:08 -0300

    Add Kassel Labs Logo.

--- src/js/components/PageContainer.js
+++ src/js/components/PageContainer.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import kasselLogo from '../../assets/img/KasselLabsLogo.png';
+
 const PageContainer = ({ children }) => (
   <div className="page-container">
     <div className="background-image" />
+    <div className="kasselLogo">
+      <a href="https://kassellabs.io" target="_blank" rel="noopener noreferrer">
+        <img src={kasselLogo} alt="Kassel Labs" height="20" />
+      </a>
+    </div>
     <div className="center-content">
       <h1 className="app-title">WESTWORLD<br />INTRO CREATOR</h1>
       <div>
