bd9bc32187bbdd24aedd31377d930102fb06c58b
BrOrlandi <brorlandi@gmail.com>
2018-06-19 01:07:41 -0300

    Show NavBar in pagecontainer.

--- src/js/components/PageContainer.js
+++ src/js/components/PageContainer.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import kasselLogo from '../../assets/img/KasselLabsLogo.png';
+import NavBar from './NavBar';
+import stic from '../../assets/img/stic.jpg';
+import swic from '../../assets/img/swic.jpg';
 
 const PageContainer = ({ children }) => (
   <div className="page-container">
     <div className="background-image" />
-    <div className="kasselLogo">
-      <a href="https://kassellabs.io" target="_blank" rel="noopener noreferrer">
-        <img src={kasselLogo} alt="Kassel Labs" height="20" />
-      </a>
-    </div>
+    <NavBar />
     <div className="center-content">
-      <h1 className="app-title">WESTWORLD<br />INTRO CREATOR</h1>
+      <h1 className="app-title">WESTWORLD INTRO CREATOR</h1>
       <div>
         {children}
       </div>
