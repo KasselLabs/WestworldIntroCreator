73f91d05e5f8e890a9d047440cad2de87977412c
BrOrlandi <brorlandi@gmail.com>
2018-06-21 01:13:34 -0300

    Create donate page.

--- src/js/components/Routes.js
+++ src/js/components/Routes.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import { HashRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import VideoPage from './VideoPage';
 import CreatePage from './CreatePage';
 import EditPage from './EditPage';
 import DownloadPage from './DownloadPage';
+import DonatePage from './DonatePage';
 
 const Routes = () => (
   <Router>
-    <div>
+    <Switch>
       <Route exact path="/" component={CreatePage} />
+      <Route exact path="/donate" component={DonatePage} />
       <Route exact path="/:openingKey" component={VideoPage} />
       <Route path="/:openingKey/edit" component={EditPage} />
       <Route path="/:openingKey/download" component={DownloadPage} />
-    </div>
+    </Switch>
   </Router>
 );
 
