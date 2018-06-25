b17e1ff18c0987d5627a3089571966f462ab0c52
BrOrlandi <brorlandi@gmail.com>
2018-06-25 01:27:14 -0300

    Show alert for iOS devices.

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,7 @@ import '../styles/index.styl';
 import { documentReady } from './extras/utils';
 import renderApp from './components/renderApp';
 import './extras/googleanalytics';
+import Swal from './extras/swal';
 
 (function _() {
   if ('development' === process.env.NODE_ENV) {
@@ -26,3 +27,14 @@ function _startApplication() {
   };
 }
 
+const iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+const westworldIOS = 'WestworldIOSModal';
+const alreadyShow = localStorage.getItem(westworldIOS);
+
+if (iOS && !alreadyShow) {
+  Swal('IOS DEVICE', `This Website may not work properly on iOS devices, the texts on the video may not be shown.
+    For better experience try it on a desktop computer or another device.
+    Sorry for the inconvenience. We are trying to fix it soon.
+  `);
+  localStorage.setItem(westworldIOS, 'showed');
+}
