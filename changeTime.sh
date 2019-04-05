eed8f18b6b9d590c6d6083465f16808a7cb63f6b
BrOrlandi <brorlandi@gmail.com>
2019-04-05 13:43:30 -0300

    Update change time script.

--- changeTime.sh
+++ changeTime.sh
@@ -1,2 +1,2 @@
-sed -i -e "s/export const TIME_FACTOR = .*;/export const TIME_FACTOR = $1;/g" src/js/api/config.js
-sed -i -e "s/\$timeFactor = .*;/\$timeFactor = $1;/g" src/styles/animations.styl
+sed -i '' -e "s/export const TIME_FACTOR = .*;/export const TIME_FACTOR = $1;/g" src/js/api/config.js
+sed -i '' -e "s/\$timeFactor = .*;/\$timeFactor = $1;/g" src/styles/animations.styl
