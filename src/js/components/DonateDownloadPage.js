31e69fa3a8e462c186c5e5924128bc2022514343
BrOrlandi <brorlandi@gmail.com>
2019-06-17 02:52:49 -0300

    Improve DonateDownloadPage.

--- src/js/components/DonateDownloadPage.js
+++ src/js/components/DonateDownloadPage.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import Loader from './Loader';
@@ -8,42 +8,39 @@ import ContactButton from './ContactButton';
 import TermsOfServiceAcceptance from './TermsOfServiceAcceptance';
 import EmailRequestField from './download/EmailRequestField';
 
-// eslint-disable-next-line react/prefer-stateless-function
-class DonateDownloadPage extends Component {
-  render() {
-    const { match: { params } } = this.props;
-    const { openingKey } = params;
+const DonateDownloadPage = ({ match }) => {
+  const { params } = match;
+  const { openingKey } = params;
 
-    return (
-      <DownloadPageContainer title="DONATE AND DOWNLOAD">
-        <p>
-          Great choice! You can donate how much you want but there
-          is the minimum of <span className="bold">10 US Dollars</span> to receive the video.
-        </p>
-        <div className="compose-iframe">
-          <div className="center center-content">
-            <Loader />
-          </div>
-          <iframe
-            className="stripe"
-            id="stripeDonateIframe"
-            title="Stripe Payment Form"
-            src={`${paymentPageUrl}?embed=true&app=westworld&code=${openingKey}`}
-            allowpaymentrequest="true"
-          />
+  return (
+    <DownloadPageContainer title="DONATE AND DOWNLOAD">
+      <p>
+        Great choice! You can donate how much you want but there
+        is the minimum of <span className="bold">10 US Dollars</span> to receive the video.
+      </p>
+      <div className="compose-iframe">
+        <div className="center center-content">
+          <Loader />
         </div>
-        <p>
-          You will receive the confirmation and the video
-          on the email you put in the form above.
-          Please, confirm your email below.
-        </p>
-        <ContactButton />
-        <TermsOfServiceAcceptance />
-        <EmailRequestField />
-      </DownloadPageContainer>
-    );
-  }
-}
+        <iframe
+          className="stripe"
+          id="stripeDonateIframe"
+          title="Stripe Payment Form"
+          src={`${paymentPageUrl}?embed=true&app=westworld&code=${openingKey}`}
+          allowpaymentrequest="true"
+        />
+      </div>
+      <p>
+        You will receive the confirmation and the video
+        on the email you put in the form above.
+        Please, confirm your email below.
+      </p>
+      <ContactButton />
+      <TermsOfServiceAcceptance />
+      <EmailRequestField />
+    </DownloadPageContainer>
+  );
+};
 
 DonateDownloadPage.propTypes = {
   match: PropTypes.object,
