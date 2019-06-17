9aa9b8790d341a17b6e22b1da801b356c9c83f9d
BrOrlandi <brorlandi@gmail.com>
2019-06-17 02:57:26 -0300

    Minor refactor.

--- src/js/components/download/NotQueuedStatus.js
+++ src/js/components/download/NotQueuedStatus.js
@@ -26,16 +26,23 @@ const NotQueuedStatus = ({ match, status: { queueSize } }) => {
         We want to provide the video for free, but we have costs with the servers.
       </p>
       <p>
-      There are <span className="bold">{queueSize} videos</span> in front of you to be rendered and may take up to <span className="bold">{etaText}</span> to send your video.
+        There are <span className="bold">{queueSize} videos</span> in front of you
+        to be rendered and may take up to <span className="bold">{etaText}</span> to send your video.
       </p>
-      <p>Can{'\''}t wait for it? Donate at least <span className="bold">10 US Dollars</span> to support our service and your video will be ready in few hours (2 hours usually). The video is rendered in Full HD quality.</p>
-      <p>You can donate via Credit Card or PayPal. If you don{'\''}t receive the video please contact us via email so we can check it out.</p>
+      <p>
+        Can{'\''}t wait for it? Donate at least <span className="bold">10 US Dollars</span> to support our service and your video will be ready in few hours (2 hours usually).
+        The video is rendered in Full HD quality.
+      </p>
+      <p>
+        You can donate via Credit Card or PayPal.
+        If you don{'\''}t receive the video please contact us via email so we can check it out.
+      </p>
       <p><span className="bold">Attention!</span> Before sending the download request make sure there are no typos in your text to grant that your video will be with the correct text.{' '}
         <Link to={`/${openingKey}/edit`}>Click here to go back and check your text.</Link>
       </p>
       <ContactButton />
       <TermsOfServiceAcceptance />
-      <DonateOrNotDonate openingKey={openingKey} />
+      <DonateOrNotDonate />
     </Fragment>
   );
 };
