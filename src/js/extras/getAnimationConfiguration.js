a832b2cc729d1bea1373d290615ec6a2edbd0d1b
BrOrlandi <brorlandi@gmail.com>
2019-08-22 01:09:57 -0300

    Fix fullscreen mode for mobile.

--- src/js/extras/getAnimationConfiguration.js
+++ src/js/extras/getAnimationConfiguration.js
@@ -1,12 +1,22 @@
+import bowser from 'bowser';
 import { getDefaultAnimationConfig, baseWidthReference } from '../animationData/animationConfig';
+
+const browser = bowser.getParser(window.navigator.userAgent);
 
 const VIDEO_RATIO = 16 / 9;
 
 const px = value => `${value}px`;
 
-const isFullscreen = () => (
-  window.innerWidth === window.screen.width && window.innerHeight === window.screen.height
-);
+const isFullscreen = () => {
+  const fullscreenContainer = document.querySelector('.FullScreen');
+  if (browser.is('mobile')) {
+    return window.innerWidth === fullscreenContainer.offsetWidth
+    && window.innerHeight === fullscreenContainer.offsetHeight;
+  }
+
+  return window.innerWidth === window.screen.width
+  && window.innerHeight === window.screen.height;
+};
 
 const calcRelativeValue = (value, videoWidth) => px((value / baseWidthReference) * videoWidth);
 
@@ -38,6 +48,13 @@ const calcVideoSize = (viewportHeight, viewportWidth) => {
 
 const calcViewportSize = (videoContainer) => {
   if (isFullscreen()) {
+    if (browser.is('mobile')) {
+      return {
+        height: window.innerHeight,
+        width: window.innerWidth,
+      };
+    }
+
     return {
       height: window.screen.height,
       width: window.screen.width,
