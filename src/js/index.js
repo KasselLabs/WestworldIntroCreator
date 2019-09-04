dbe34507406f8fb70c1339d6ca01f7f4839a97a5
Nihey Takizawa <nihey.takizawa@gmail.com>
2019-09-04 00:40:43 -0300

    Do not display "iOS Warning" message anymore

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,6 @@ import '../styles/index.styl';
 import { documentReady } from './extras/utils';
 import renderApp from './renderApp';
 import './extras/googleanalytics';
-import Swal from './extras/swal';
 
 (function _() {
   if ('development' === process.env.NODE_ENV) {
@@ -25,16 +24,4 @@ function _startApplication() {
   window.onresize = () => {
     renderApp();
   };
-}
-
-const iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
-const westworldIOS = 'WestworldIOSModal';
-const alreadyShow = localStorage.getItem(westworldIOS);
-
-if (iOS && !alreadyShow) {
-  Swal('IOS DEVICE', `This Website may not work properly on iOS devices, the texts on the video may not be shown.
-    For better experience try it on a desktop computer or another device.
-    Sorry for the inconvenience. We are trying to fix it soon.
-  `);
-  localStorage.setItem(westworldIOS, 'showed');
 }
