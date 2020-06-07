732a2b268c9a175a87e383d10917314e088c7f57
BrOrlandi <brorlandi@gmail.com>
2020-06-07 14:15:17 -0300

    Add default amount to donate.

--- src/js/pages/DonateDownloadPage.js
+++ src/js/pages/DonateDownloadPage.js
@@ -29,7 +29,7 @@ const DonateDownloadPage = ({ match }) => {
           className="stripe"
           id="stripeDonateIframe"
           title="Stripe Payment Form"
-          src={`${paymentPageUrl}?embed=true&app=westworld&code=${openingKey}`}
+          src={`${paymentPageUrl}?embed=true&app=westworld&code=${openingKey}&amount=1000`}
           allowpaymentrequest="true"
         />
       </div>
