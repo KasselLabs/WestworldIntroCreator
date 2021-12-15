423e5db1e45e5064a30fcb44d3fcb11ca5556441
Nihey Takizawa <nihey.takizawa@gmail.com>
2021-12-15 17:05:48 -0300

    Add Donation Options buttons on DonateDownloadPage

---
+++ src/js/DonationOptions.js
@@ -0,0 +1,61 @@
+import React, { useState, useCallback } from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+
+const DONATION_AMOUNTS = {
+  watermark: 10,
+  'no-watermark': 20,
+};
+
+export default function DonationOptions({ updatePaymentAmount }) {
+  const [selectedOption, setSelectedOption] = useState('no-watermark');
+
+  const selectOption = useCallback((option) => {
+    setSelectedOption(option);
+    updatePaymentAmount(DONATION_AMOUNTS[option]);
+  });
+
+  return (
+    <div className="donation-options">
+      <button
+        type="button"
+        className={classnames('option', {
+          '-selected': 'watermark' === selectedOption,
+        })}
+        onClick={() => selectOption('watermark')}
+      >
+        <span className="title">STANDARD</span>
+        <span className="description">Full HD Quality</span>
+        <span className="description">MP4 Video</span>
+        <span className="description">Video will be like in the preview</span>
+        <span className="description">
+          Donate at least
+          {' '}
+          <b>$10</b>
+        </span>
+      </button>
+      <button
+        type="button"
+        className={classnames('option', {
+          '-selected': 'no-watermark' === selectedOption,
+        })}
+        onClick={() => selectOption('no-watermark')}
+      >
+        <span className="title">NO WATERMARK</span>
+        <span className="description">Full HD Quality</span>
+        <span className="description">MP4 Video</span>
+        <span className="description">Video will be like in the preview</span>
+        <span className="description">but without the watermark</span>
+        <span className="description">
+          Donate at least
+          {' '}
+          <b>$20</b>
+        </span>
+      </button>
+    </div>
+  );
+}
+
+DonationOptions.propTypes = {
+  updatePaymentAmount: PropTypes.func.isRequired,
+};
